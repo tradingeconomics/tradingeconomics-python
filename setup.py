a7e0a851a8c5d8472ed449deb7d740880fe5e733
TETatenda <tatenda@tradingeconomics.com>
2026-02-17 11:58:10 +0200

    v4.5.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 setup(
     name="tradingeconomics",
     packages=find_packages(exclude=["tests*"]),
-    version="4.5.8",
+    version="4.5.9",
     description="Trading Economics API",
     long_description=readme(),
     long_description_content_type="text/x-rst",
